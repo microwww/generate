160b72e7504aa745948f4262658e5f35534ced2b
lichangshu <lcs.005@163.com>
2019-11-14 19:30:04 +0800

    writerEntitySetToList

--- src/main/java/com/github/microwww/generate/JpaEntity.java
+++ src/main/java/com/github/microwww/generate/JpaEntity.java
@@ -3,8 +3,12 @@ package com.github.microwww.generate;
 import com.github.javaparser.StaticJavaParser;
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.body.FieldDeclaration;
+import com.github.javaparser.ast.body.Parameter;
+import com.github.javaparser.ast.body.VariableDeclarator;
 import com.github.javaparser.ast.expr.NormalAnnotationExpr;
+import com.github.javaparser.ast.type.ClassOrInterfaceType;
 import com.github.microwww.generate.util.FileHelper;
+import com.github.microwww.generate.util.ParserHelper;
 import org.apache.openjpa.jdbc.conf.JDBCConfiguration;
 import org.apache.openjpa.jdbc.conf.JDBCConfigurationImpl;
 import org.apache.openjpa.jdbc.meta.ReverseMappingTool;
@@ -16,12 +20,14 @@ import org.apache.openjpa.persistence.PersistenceUnitInfoImpl;
 
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.sql.DataSource;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class JpaEntity {
 
@@ -102,6 +108,57 @@ public class JpaEntity {
                     return Optional.empty();
                 }));
                 return ooo.orElse(Optional.empty()).orElse(Optional.empty());
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
+
+    public List<File> writerEntitySetToList() {
+        return FileHelper.writeJavaFile(srcDirectory, entitySetToList());
+    }
+
+    public List<CompilationUnit> entitySetToList() {
+        return FileHelper.scanJavaFile(srcDirectory, file -> {
+            try {
+                CompilationUnit parse = StaticJavaParser.parse(file);
+                String name = file.getName().split("\\.")[0];
+                AtomicBoolean exist = new AtomicBoolean();
+                parse.getClassByName(name)
+                        .ifPresent(clazz -> {
+                            clazz.getAnnotationByName("Entity")
+                                    .ifPresent(o -> {
+                                        for (FieldDeclaration field : clazz.getFields()) {
+                                            field.getAnnotationByClass(OneToMany.class).ifPresent(e -> {
+                                                NormalAnnotationExpr ex = (NormalAnnotationExpr) e;
+                                                ex.getPairs().stream().filter(f -> f.getNameAsString().equals("targetEntity")).findAny().ifPresent(val -> {
+                                                    val.getValue().toClassExpr().ifPresent(expr -> {
+                                                        ClassOrInterfaceType type = StaticJavaParser.parseClassOrInterfaceType(List.class.getSimpleName());
+                                                        ClassOrInterfaceType ci = (ClassOrInterfaceType) expr.getType();
+                                                        type.setTypeArguments(ci.removeScope());
+                                                        field.setVariable(0, new VariableDeclarator(type, field.getVariable(0).getName()));
+                                                        exist.getAndSet(true);
+
+                                                        clazz.getMethodsByName("get" + ParserHelper.firstUpper(field.getVariable(0).getNameAsString())).forEach(m -> {
+                                                            if (m.getParameters().isEmpty()) {
+                                                                m.setType(type);
+                                                            }
+                                                        });
+                                                        clazz.getMethodsByName("set" + ParserHelper.firstUpper(field.getVariable(0).getNameAsString())).forEach(m -> {
+                                                            if (m.getParameters().size() == 1) {
+                                                                m.setParameter(0, new Parameter(type, m.getParameter(0).getName()));
+                                                            }
+                                                        });
+                                                    });
+                                                });
+                                            });
+                                        }
+                                    });
+                        });
+                if (!exist.get()) {
+                    parse = null;
+                }
+                return Optional.ofNullable(parse);
             } catch (FileNotFoundException e) {
                 throw new RuntimeException(e);
             }
