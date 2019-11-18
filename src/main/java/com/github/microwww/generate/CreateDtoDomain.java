9691984275450cddfca91e843010f2bb6f9ecd69
lichangshu <lcs.005@163.com>
2019-11-18 15:34:07 +0800

    createConversionMethod

--- src/main/java/com/github/microwww/generate/CreateDtoDomain.java
+++ src/main/java/com/github/microwww/generate/CreateDtoDomain.java
@@ -1,14 +1,10 @@
 package com.github.microwww.generate;
 
 import com.github.javaparser.StaticJavaParser;
-import com.github.javaparser.ast.CompilationUnit;
-import com.github.javaparser.ast.ImportDeclaration;
-import com.github.javaparser.ast.Modifier;
-import com.github.javaparser.ast.NodeList;
+import com.github.javaparser.ast.*;
 import com.github.javaparser.ast.body.*;
 import com.github.javaparser.ast.expr.*;
-import com.github.javaparser.ast.stmt.BlockStmt;
-import com.github.javaparser.ast.stmt.ReturnStmt;
+import com.github.javaparser.ast.stmt.*;
 import com.github.javaparser.ast.type.ClassOrInterfaceType;
 import com.github.javaparser.ast.type.TypeParameter;
 import com.github.microwww.generate.util.FileHelper;
@@ -20,6 +16,8 @@ import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 import java.util.regex.Matcher;
@@ -41,11 +39,91 @@ public class CreateDtoDomain {
         BlockStmt body = cstr.getBody();
         body.addStatement(new AssignExpr(new FieldAccessExpr(new ThisExpr(), field.getVariable(0).getNameAsString()), new NameExpr(domain.getName()), AssignExpr.Operator.ASSIGN));
 
+        createGetOriginMethod(clazz, classParam, field);
+        createConversionMethod(clazz);
+
+        return unit;
+    }
+
+    private static void createGetOriginMethod(ClassOrInterfaceDeclaration clazz, TypeParameter classParam, FieldDeclaration field) {
         MethodDeclaration method = clazz.addMethod("origin", Modifier.Keyword.PUBLIC);
         method.setType(classParam);
         method.getBody().get().addStatement(new ReturnStmt(new FieldAccessExpr(new ThisExpr(), field.getVariable(0).getNameAsString())));
+    }
 
-        return unit;
+    /**
+     * <pre>
+     *     public static &lt; T, V extends AbstractDomainValue &lt; T &gt; &gt; List &lt; V &gt;  conversion(Class &lt; V &gt;  clazz, Collection &lt; T &gt;  list) {
+     *         return page.stream().map((T m) - &gt;  {
+     *             try {
+     *                 return clazz.getConstructor(new Class[] { m.getClass() }).newInstance(m);
+     *             } catch (NoSuchMethodException | InstantiationException | IllegalAccessException | InvocationTargetException e) {
+     *                 throw new UnsupportedOperationException("Need one Constructor with one  &lt; T &gt;  param", e);
+     *             }
+     *         });
+     *     }
+     * </pre>
+     **/
+    private static void createConversionMethod(ClassOrInterfaceDeclaration clazz) {
+        // public static <T, V extends AbstractDomainValue<T &gt;  &gt;  List<V &gt;  conversion(Class<V &gt;  clazz, Collection<T &gt;  list) {
+        MethodDeclaration method = clazz.addMethod("conversion", Modifier.Keyword.PUBLIC, Modifier.Keyword.STATIC);
+        method.setType(new ClassOrInterfaceType().setName("List").setTypeArguments(new ClassOrInterfaceType().setName("V")));
+        ClassOrInterfaceType ex = StaticJavaParser.parseClassOrInterfaceType("AbstractDomainValue");
+        ex.setTypeArguments(new TypeParameter("T"));
+        method.setTypeParameters(new NodeList<>(
+                new TypeParameter("T"),
+                new TypeParameter("V",
+                        new NodeList(ex))));
+        String parma1 = "clazz";
+        String parma2 = "list";
+        method.addParameter(
+                new ClassOrInterfaceType()
+                        .setName("Class")
+                        .setTypeArguments(new TypeParameter("V")),
+                parma1);
+        method.addParameter(new ClassOrInterfaceType()
+                        .setName("Collection")
+                        .setTypeArguments(new TypeParameter("T")),
+                parma2);
+
+        method.tryAddImportToParentCompilationUnit(Collection.class);
+        method.tryAddImportToParentCompilationUnit(List.class);
+        method.tryAddImportToParentCompilationUnit(InstantiationException.class);
+        method.tryAddImportToParentCompilationUnit(InvocationTargetException.class);
+        method.tryAddImportToParentCompilationUnit(Collectors.class);
+
+        String lambParam = "m";
+        //page.stream().map(m - &gt;  {
+        method.getBody().get().addStatement(new ReturnStmt( // return
+                new MethodCallExpr(new MethodCallExpr(new MethodCallExpr(new NameExpr(parma2), "stream"), // page.stream(..)
+                        "map", new NodeList<>( // map( ... )
+                        new LambdaExpr(
+                                new NodeList(new Parameter(new ClassOrInterfaceType().setName("T"), lambParam)),
+                                new BlockStmt().addStatement(
+                                        new TryStmt(
+                                                new BlockStmt().addStatement(new ReturnStmt(//return clazz.getConstructor(new Class[]{m.getClass()}).newInstance(m);
+                                                        new MethodCallExpr(
+                                                                new MethodCallExpr(new NameExpr(parma1), "getConstructor", //clazz.getConstructor(....)
+                                                                        new NodeList<>(new ArrayCreationExpr(new ClassOrInterfaceType().setName("Class"), // new Class
+                                                                                new NodeList<>(new ArrayCreationLevel()), // []
+                                                                                new ArrayInitializerExpr(new NodeList<>( // { ...
+                                                                                        new MethodCallExpr(new NameExpr(lambParam), "getClass") // m.getClass()
+                                                                                )) // }
+                                                                        ))
+                                                                ), // new Class[]{m.getClass()}
+                                                                "newInstance", new NodeList<>(new NameExpr(lambParam))) // .newInstance(m)
+                                                )),
+                                                new NodeList<>(new CatchClause(new Parameter(new ClassOrInterfaceType().setName("NoSuchMethodException | InstantiationException | IllegalAccessException | InvocationTargetException"),
+                                                        "e"),
+                                                        new BlockStmt().addStatement(new ThrowStmt(new ObjectCreationExpr()
+                                                                .setType("UnsupportedOperationException").setArguments(new NodeList<>(
+                                                                        new StringLiteralExpr("Need one Constructor with one <T> param"), new NameExpr("e"))))))),
+                                                null
+                                        )
+                                )
+                        ))
+                ), "collect", new NodeList<>(new MethodCallExpr(new NameExpr("Collectors"), "toList")))
+        ));
     }
 
     public static List<CompilationUnit> createEntityDTO(File src, String pkg) {
@@ -148,7 +226,7 @@ public class CreateDtoDomain {
             BlockStmt stmt = md.getBody().get().setStatements(new NodeList<>());
             stmt.addStatement(new ReturnStmt(new ObjectCreationExpr(
                     null, change, new NodeList<>(new MethodCallExpr(new FieldAccessExpr(
-                            new SuperExpr(), "domain"), method.getName())))));
+                    new SuperExpr(), "domain"), method.getName())))));
 
         }
 
@@ -174,7 +252,7 @@ public class CreateDtoDomain {
                             AssignExpr.Operator.ASSIGN));
                     // return list.stream().map(GoPlayerValue.Simple::new).collect(Collectors.toList());
                     Class impt = Collectors.class;
-                    ParserHelper.getRootNode(toClazz).ifPresent( ff ->{
+                    ParserHelper.getRootNode(toClazz).ifPresent(ff -> {
                         ff.addImport(impt);
                     });
                     stmt.addStatement(new ReturnStmt(
