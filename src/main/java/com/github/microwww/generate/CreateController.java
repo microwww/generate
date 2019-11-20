e5819ab8d83ffb8bb6d2ae900e4143c0ae2a6868
lichangshu <lcs.005@163.com>
2019-11-20 10:54:35 +0800

    create controller

--- src/main/java/com/github/microwww/generate/CreateController.java
+++ src/main/java/com/github/microwww/generate/CreateController.java
@@ -1,15 +1,16 @@
 package com.github.microwww.generate;
 
+import com.github.javaparser.StaticJavaParser;
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.Modifier;
 import com.github.javaparser.ast.NodeList;
-import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
-import com.github.javaparser.ast.body.FieldDeclaration;
-import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.body.*;
 import com.github.javaparser.ast.expr.*;
+import com.github.javaparser.ast.nodeTypes.NodeWithName;
 import com.github.javaparser.ast.stmt.ReturnStmt;
 import com.github.javaparser.ast.type.ClassOrInterfaceType;
 import com.github.javaparser.ast.type.PrimitiveType;
+import com.github.microwww.generate.util.FileHelper;
 import com.github.microwww.generate.util.ParserHelper;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,44 +19,67 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class CreateController {
 
-    public static String toURI(String name) {
-        return ParserHelper.firstLower(name).replaceAll("([A-Z])", "-$1").toLowerCase();
+    private final File src;
+    private final ClassOrInterfaceDeclaration entity;
+    private ClassOrInterfaceDeclaration _service;
+
+    public CreateController(File src, ClassOrInterfaceDeclaration entity) {
+        this.src = src;
+        this.entity = entity;
     }
 
-    public static CompilationUnit createClass(String pkg, ClassOrInterfaceDeclaration entity, ClassOrInterfaceDeclaration service) {
+    public synchronized ClassOrInterfaceDeclaration getService() {
+        if (_service == null) {
+            _service = searchService().orElseGet(() -> {
+                TypeDeclaration clazz = StaticJavaParser.parse("public class " + entity.getNameAsString() + "Service {}").getType(0);
+                return (ClassOrInterfaceDeclaration) clazz;
+            });
+        }
+        return _service;
+    }
+
+    public CompilationUnit createClass(String pkg) {
         CompilationUnit unit = new CompilationUnit(pkg);
         ClassOrInterfaceDeclaration clazz = unit.addClass(entity.getNameAsString() + "Controller", Modifier.Keyword.PUBLIC);
         clazz.addAndGetAnnotation(RestController.class);
         clazz.addAndGetAnnotation(RequestMapping.class).addPair("value", new StringLiteralExpr("/" + toURI(entity.getNameAsString())));
 
+        ClassOrInterfaceDeclaration service = this.getService();
+        unit.addImport(ParserHelper.findImportClass(service));
+
         FieldDeclaration field = clazz.addField(service.getNameAsString(), ParserHelper.firstLower(service.getNameAsString())).addAnnotation("Autowired");
         field.tryAddImportToParentCompilationUnit(Autowired.class);
-        createListMethod(entity, clazz, field);
-        createDetailMethod(entity, clazz, field);
-        createSaveMethod(entity, clazz, field);
+        createListMethod(clazz, field);
+        createDetailMethod(clazz, field);
+        createSaveMethod(clazz, field);
 
         unit.addImport(ParserHelper.findImportClass(entity));
         unit.addImport(ParserHelper.findImportClass(service));
+        unit.getImports().sort(Comparator.comparing(NodeWithName::getNameAsString));
 
         return unit;
     }
 
     /**
-     *     @PostMapping("/save")
+     * <pre>
      *     public void saveGoTeam(GoTeam pay) {
-     *         Optional<GoTeam> db = goTeamService.findById(pay.getId());
+     *         Optional&lt;GoTeam&gt; db = goTeamService.findById(pay.getId());
      *         GoTeam entity = db.orElse(pay);
      *         BeanUtils.copyProperties(pay, entity, "id");
      *         goTeamService.save(entity);
      *     }
+     *     </pre>
      */
-    private static void createSaveMethod(ClassOrInterfaceDeclaration entity, ClassOrInterfaceDeclaration clazz, FieldDeclaration field) {
+    private void createSaveMethod(ClassOrInterfaceDeclaration clazz, FieldDeclaration field) {
         String param = "entity";
         MethodDeclaration method = clazz.addMethod("save" + entity.getNameAsString(), Modifier.Keyword.PUBLIC);
         method.addParameter(entity.getNameAsString(), param);
@@ -89,21 +113,24 @@ public class CreateController {
 
     /**
      * <pre>
-     *     @GetMapping("/detail")
+     *     &#64;GetMapping("/detail")
      *     public GoShop detailGoShop(int id) {
      *         return goShopService.findById(id).orElseGet(null);
      *     }
      * </pre>
      *
-     * @param entity
      * @param clazz
      * @param field
      */
-    private static void createDetailMethod(ClassOrInterfaceDeclaration entity, ClassOrInterfaceDeclaration clazz, FieldDeclaration field) {
+    private void createDetailMethod(ClassOrInterfaceDeclaration clazz, FieldDeclaration field) {
         MethodDeclaration method = clazz.addMethod("detail" + entity.getNameAsString(), Modifier.Keyword.PUBLIC);
         method.setType(entity.getNameAsString() + "Value.Simple").addAndGetAnnotation(GetMapping.class).addPair("value", new StringLiteralExpr("/detail"));
 
-        //method.tryAddImportToParentCompilationUnit();
+        searchDTO().ifPresent(c -> {
+            ParserHelper.getRootNode(clazz).ifPresent(o -> {
+                o.addImport(ParserHelper.findImportClass(c));
+            });
+        });
 
         Optional<FieldDeclaration> id = ParserHelper.findFieldByAnnotation(entity, "Id");
         method.addAndGetParameter(id.get().getElementType(), "id");
@@ -121,7 +148,7 @@ public class CreateController {
 
     /**
      * <pre>
-     * @GetMapping("/list")
+     * &#64;GetMapping("/list")
      * public List &lt; GoTeamValue.Simple> listGoTeam(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int size) {
      *     return goTeamService.findAll(page, size).stream()
      *         .map(GoTeamValue.Simple::new)
@@ -129,11 +156,10 @@ public class CreateController {
      * }
      * </pre>
      *
-     * @param entity
      * @param clazz
      * @param field
      */
-    private static void createListMethod(ClassOrInterfaceDeclaration entity, ClassOrInterfaceDeclaration clazz, FieldDeclaration field) {
+    private void createListMethod(ClassOrInterfaceDeclaration clazz, FieldDeclaration field) {
         MethodDeclaration method = clazz.addMethod("list" + entity.getNameAsString(), Modifier.Keyword.PUBLIC);
         //method.tryAddImportToParentCompilationUnit(List.class);
         method.setType(new ClassOrInterfaceType().setName("List").setTypeArguments(new ClassOrInterfaceType().setName(entity.getNameAsString() + "Value.Simple")));
@@ -153,5 +179,56 @@ public class CreateController {
         method.tryAddImportToParentCompilationUnit(Collectors.class);
     }
 
+    public Optional<ClassOrInterfaceDeclaration> searchDTO() {
+        String className = entity.getNameAsString() + "Value";
+        Optional<CompilationUnit> clazz = this.searchClass(className);
+        //clazz.get().get
+        Optional<ClassOrInterfaceDeclaration> simple = clazz.map(m -> {
+            for(TypeDeclaration<?> type : m.getTypes()){
+                for(BodyDeclaration<?> yp : type.getMembers()){
+                    if(yp.isClassOrInterfaceDeclaration()){
+                        ClassOrInterfaceDeclaration dec = (ClassOrInterfaceDeclaration) yp;
+                        if(dec.getNameAsString().equals("Simple")){
+                            return Optional.of(dec);
+                        }
+                    }
+                }
+            }
+            return Optional.ofNullable((ClassOrInterfaceDeclaration) null);
+        }).orElse(Optional.empty());
+        if (simple.isPresent()) {
+            return clazz.map(o -> o.getClassByName(className)).orElse(Optional.empty());
+        }
+        return Optional.empty();
+    }
+
+    public Optional<ClassOrInterfaceDeclaration> searchService() {
+        String className = entity.getNameAsString() + "Service";
+        return this.searchClass(className).map(m -> {
+            return m.getClassByName(className);
+        }).orElse(Optional.empty());
+    }
+
+    public Optional<CompilationUnit> searchClass(String className) {
+        //String className = entity.getNameAsString() + "Service";
+        String fileName = className + ".java";
+        List<File> files = FileHelper.scanJavaFile(src, (f) -> {
+            if (f.getName().equalsIgnoreCase(fileName)) {
+                return Optional.of(f);
+            }
+            return Optional.empty();
+        });
+        if (!files.isEmpty()) {
+            try {
+                return Optional.of(StaticJavaParser.parse(files.get(0)));
+            } catch (FileNotFoundException e) {
+            }
+        }
+        return Optional.empty();
+    }
+
+    public static String toURI(String name) {
+        return ParserHelper.firstLower(name).replaceAll("([A-Z])", "-$1").toLowerCase();
+    }
 
 }
