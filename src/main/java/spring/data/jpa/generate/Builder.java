0398644c1436db8242ebc011702d10c68cebb2c5
changshu.li <lcs.005@163.com>
2020-11-18 11:11:20 +0800

    sort by database

--- src/main/java/spring/data/jpa/generate/Builder.java
+++ src/main/java/spring/data/jpa/generate/Builder.java
@@ -1,22 +1,37 @@
 package spring.data.jpa.generate;
 
+import com.github.javaparser.ast.NodeList;
+import com.github.javaparser.ast.body.BodyDeclaration;
+import com.github.javaparser.ast.expr.AnnotationExpr;
+import com.github.javaparser.ast.expr.MemberValuePair;
+import com.github.javaparser.ast.expr.NormalAnnotationExpr;
 import org.apache.openjpa.jdbc.conf.JDBCConfiguration;
-import org.apache.openjpa.jdbc.conf.JDBCConfigurationImpl;
 import org.apache.openjpa.jdbc.meta.ReverseMappingTool;
 import org.apache.openjpa.lib.conf.Configurations;
 import org.apache.openjpa.lib.util.Options;
 import org.apache.openjpa.persistence.EntityManagerFactoryImpl;
 import org.apache.openjpa.persistence.PersistenceProviderImpl;
 import org.apache.openjpa.persistence.PersistenceUnitInfoImpl;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
+import javax.persistence.Column;
+import javax.persistence.Table;
 import javax.sql.DataSource;
 import java.io.File;
-import java.util.Collections;
-import java.util.List;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.*;
 
 public class Builder {
 
     private JDBCConfiguration conf;
+    private DataSource dataSource;
     private File srcDirectory;
 
     public Builder(DataSource dataSource) {
@@ -26,17 +41,17 @@ public class Builder {
         unit.setPersistenceUnitName("default");
         EntityManagerFactoryImpl factory = (EntityManagerFactoryImpl) provider.createContainerEntityManagerFactory(unit, Collections.EMPTY_MAP);
         conf = (JDBCConfiguration) factory.getConfiguration();
-        //db2entity(config, pkg);
+        this.dataSource = dataSource;
     }
 
     public Builder(String driver, String url, String username, String password) {
-        conf = new JDBCConfigurationImpl();
-        conf.setConnectionDriverName(driver);
-        conf.setConnectionURL(url);
-        conf.setConnectionUserName(username);
-        conf.setConnectionPassword(password);
-        conf.setSpecification("jpa");
-        //db2entity(conf, pkg);
+        this(Builder.dataSource(driver, url, username, password));
+    }
+
+    private static DataSource dataSource(String driver, String url, String username, String password) {
+        DriverManagerDataSource ds = new DriverManagerDataSource(url, username, password);
+        ds.setDriverClassName(driver);
+        return ds;
     }
 
     public File getSrcDirectory() {
@@ -60,6 +75,71 @@ public class Builder {
                 conf.close();
             }
         });
+        try {
+            Connection connection = dataSource.getConnection();
+            JpaEntity.scanJavaEntity(srcDirectory).forEach(entity -> {
+                Optional<AnnotationExpr> tab = entity.getClazz().getAnnotationByClass(Table.class);
+                if (!tab.isPresent()) {
+                    return;
+                }
+                if (tab.get().isNormalAnnotationExpr()) {
+                    NormalAnnotationExpr an = tab.get().asNormalAnnotationExpr();
+                    for (MemberValuePair p : an.getPairs()) {
+                        if (!"name".equals(p.getNameAsString())) {
+                            continue;
+                        }
+                        String name = p.getValue().toStringLiteralExpr().get().asString();
+                        try {
+                            NodeList<BodyDeclaration<?>> list = sort(connection, entity, name);
+                            entity.getClazz().setMembers(list);
+                            Path path = entity.getUnit().getStorage().get().getPath();
+                            Files.write(path, entity.getUnit().toString().getBytes(), StandardOpenOption.WRITE);
+                        } catch (SQLException | IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                }
+
+            });
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
         return JpaEntity.scanJavaEntity(srcDirectory);
+    }
+
+    private NodeList<BodyDeclaration<?>> sort(Connection connection, JpaEntity entity, String name) throws SQLException {
+        // ResultSet rset = connection.getMetaData().getTables(connection.getCatalog(), connection.getSchema(), name, new String[]{"TABLE"});
+        ResultSet rset = connection.getMetaData().getColumns(connection.getCatalog(), connection.getSchema(), name, null);
+        List<String> col = new ArrayList<>();
+        while (rset.next()) {
+            col.add(rset.getString("COLUMN_NAME"));
+        }
+        NodeList<BodyDeclaration<?>> members = entity.getClazz().getMembers();
+        NodeList<BodyDeclaration<?>> list = new NodeList<>();
+        Map<String, BodyDeclaration<?>> membersMap = new TreeMap<>();
+        Iterator<BodyDeclaration<?>> iterator = members.iterator();
+        while (iterator.hasNext()) {
+            BodyDeclaration<?> e = iterator.next();
+            if (e.isFieldDeclaration()) {
+                String cName = e.asFieldDeclaration().getAnnotationByClass(Column.class).flatMap(ee -> {
+                    return ee.asNormalAnnotationExpr().getPairs()
+                            .stream().filter(eee -> "name".equals(eee.getName())).findAny()
+                            .map(eee -> eee.getValue().asLiteralStringValueExpr().getValue());
+                }).orElse(e.asFieldDeclaration().getVariables().get(0).getNameAsString());
+                membersMap.put(cName, e);
+                iterator.remove();
+            }
+        }
+        for (String s : col) {
+            BodyDeclaration<?> remove = membersMap.remove(s);
+            if (remove != null) {
+                list.add(remove);
+            }
+        }
+        list.addAll(membersMap.values());
+        for (BodyDeclaration<?> member : members) {
+            list.add(member);
+        }
+        return list;
     }
 }
