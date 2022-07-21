24bcbc9543b09e0a083840e0398d1d4128a56186
changshu.li <lcs.005@163.com>
2022-07-21 18:04:43 +0800

    test prefix

--- src/test/java/spring/data/jpa/generate/BuilderTest.java
+++ src/test/java/spring/data/jpa/generate/BuilderTest.java
@@ -12,20 +12,21 @@ public class BuilderTest {
 
     @Test
     public void createEntity() throws IOException {
-        File f = new File("C:\\Users\\changshu.li\\Desktop\\demo");
-        Map<CreateI18nException.Type, CompilationUnit> map = CreateI18nException.writeException(f, "test.exception");
-        new Builder("com.mysql.cj.jdbc.Driver", "jdbc:mysql://localhost:3306/test", "root", "123456")
-                .createEntity(f, "test.domain").stream().forEach(entity -> {
+        File f = new File("E:\\yx.demo\\4U\\src\\main\\java\\");
+        String prefix = "cn.xy.fu";
+        Map<CreateI18nException.Type, CompilationUnit> map = CreateI18nException.writeException(f, prefix + ".exception");
+        new Builder("com.mysql.cj.jdbc.Driver", "jdbc:mysql://192.168.1.246:3306/manage_4u", "root", "xyGames")
+                .createEntity(f, prefix + ".domain").stream().forEach(entity -> {
             entity.entityIdGeneratedValue();
             entity.entitySetToList();
             entity.write(f);
 
-            entity.createRepository("test.repository").ifPresent(repository -> {
+            entity.createRepository(prefix + ".repository").ifPresent(repository -> {
                 repository.addFindAll();
                 repository.addFindById();
                 repository.write(f);
 
-                SpringService service = repository.createService("test.service");
+                SpringService service = repository.createService(prefix + ".service");
                 service.addFindAll();
                 service.addFindById();
                 CompilationUnit dec = map.get(CreateI18nException.Type.ExistException);
@@ -34,17 +35,17 @@ public class BuilderTest {
                 service.addSave();
                 service.write(f);
 
-                Clazz base = ViewDomain.createBaseClass("test.vo");
+                Clazz base = ViewDomain.createBaseClass(prefix + ".vo");
                 base.write(f);
-                ViewDomain view = entity.createView("test.vo", base.toClassOrInterfaceType());
+                ViewDomain view = entity.createView(prefix + ".vo", base.toClassOrInterfaceType());
                 view.write(f);
 
-                SpringController controller = service.createController("test.controller", view);
+                SpringController controller = service.createController(prefix + ".controller", view);
                 controller.addDetailMethod();
                 controller.addListMethod();
                 controller.write(f);
             });
-            entity.createForm("test.form").write(f);
+            entity.createForm(prefix + ".form").write(f);
         });
     }
 }
